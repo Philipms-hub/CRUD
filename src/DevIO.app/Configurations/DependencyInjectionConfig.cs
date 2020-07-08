3e0fbe36931af409a3f8bc5cc0bd502475e98ee6
Philip Sousa <philipms_@hotmail.com>
2020-07-08 14:07:19 -0300

    Initial

---
+++ src/DevIO.app/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using DevIO.Data.Context;
+using Microsoft.Extensions.DependencyInjection;
+using DevIO.Business.Intefaces;
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+
+
+namespace DevIO.App.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            
+            return services;
+        }
+    }
+}
